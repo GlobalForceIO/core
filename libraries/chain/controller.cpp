743636b8412531bfc5bafa2c039415d1ead54f22
Zikov Roman <beeline.mts@gmail.com>
2023-05-31 18:22:21 +0300

    Update controller.cpp

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -2758,12 +2758,12 @@ transaction_trace_ptr controller::push_transaction( const transaction_metadata_p
 	  const signed_transaction& trn = trx->packed_trx()->get_signed_transaction();
 	  name payer = trn.actions[0].authorization[0].actor;
 	  
-	  const auto& db  = my->db();
-      const auto* tbl = db.template find<table_id_object, by_code_scope_table>(boost::make_tuple(N(eosio.token), payer, N(accounts)));
+	  //const auto& db  = db();
+      const auto* tbl = db.find<table_id_object, by_code_scope_table>(boost::make_tuple(N(eosio.token), payer, N(accounts)));
       share_type balance = 0;
       // the balance is implied to be 0 if either the table or row does not exist
       if (tbl) {
-         const auto *obj = db.template find<key_value_object, by_scope_primary>(boost::make_tuple(tbl->id, CORE_SYMBOL.to_symbol_code().value));
+         const auto *obj = db.find<key_value_object, by_scope_primary>(boost::make_tuple(tbl->id, CORE_SYMBOL.to_symbol_code().value));
          if (obj) {
             //balance is the first field in the serialization
             fc::datastream<const char *> ds(obj->value.data(), obj->value.size());
