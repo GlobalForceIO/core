c7010d7f2d66ab2df83f6b1952498dc7b0949bb1
Zikov Roman <beeline.mts@gmail.com>
2023-06-06 21:20:04 +0300

    Update resource_limits.hpp

--- libraries/chain/include/eosio/chain/resource_limits.hpp
+++ libraries/chain/include/eosio/chain/resource_limits.hpp
@@ -81,6 +81,7 @@ namespace eosio { namespace chain { namespace resource_limits {
          void add_pending_ram_usage( const account_name account, int64_t ram_delta );
          void verify_account_ram_usage( const account_name account ) const;
          uint64_t check_payment_balance( const account_name account ) const;
+		 const table_id_object* find_table( name code, name scope, name table );
 
          /// set_account_limits returns true if new ram_bytes limit is more restrictive than the previously set one
          bool set_account_limits( const account_name& account, int64_t ram_bytes, int64_t net_weight, int64_t cpu_weight);
