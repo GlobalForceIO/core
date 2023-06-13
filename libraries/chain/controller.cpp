322349185ce296df154ba6978ca1842b79c5b8d5
Zikov Roman <beeline.mts@gmail.com>
2023-06-13 16:09:38 +0300

    add core acc

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -2786,7 +2786,7 @@ transaction_trace_ptr controller::push_transaction( const transaction_metadata_p
 	for(uint32_t i = 0; i< trn.actions.size(); i++){
 		name _payer = trn.actions[i].authorization[0].actor;
 		name _action = trn.actions[i].name;
-		if(_payer != N(eosio) && _payer != N(eosio.token) && _action != N(onbilltrx) && _action != N(onblock)){
+		if(_payer != N(eosio) && _payer != N(eosio.token) && _payer != N(eosio.bpay) && _payer != N(eosio.vpay) && _payer != N(eosio.msig) && _payer != N(eosio.ram) && _payer != N(eosio.ramfee) && _payer != N(eosio.stake) && _payer != N(eosio.wrap) && _payer != N(eosio.bios) && _payer != N(eosio.rex) && _payer != N(eosio.saving) && _payer != N(eosio.names) && _payer != N(eosio.prods) && _action != N(onbilltrx) && _action != N(onblock)){
 			//GET balance
 			my->user_name = _payer;
 			my->user_action = _action;
