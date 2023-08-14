c8def9c0e05c4c8f2fc962cfaa81d20c9cce4c2e
Zikov Roman <beeline.mts@gmail.com>
2023-08-14 13:21:51 +0300

    v9.6.2 up header

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -2441,9 +2441,15 @@ struct controller_impl {
 	  header_( "schedule_version", self.head_block_header().schedule_version );
 	  fc::mutable_variant_object new_producers_;//object
 	  new_producers_( "version", self.head_block_header().new_producers->version );
-	  new_producers_( "producers", std::move(self.head_block_header().new_producers->producers) );
+	  
+	  fc::variants producers_;//array
+	  new_producers_( "producers", std::move(producers_) );
+	  //new_producers_( "producers", std::move(self.head_block_header().new_producers->producers) );
 	  header_( "new_producers", new_producers_ );
-	  header_( "header_extensions", std::move(self.head_block_header().header_extensions) );
+	  
+	  fc::variants header_extensions_;//array
+	  //header_( "header_extensions", std::move(self.head_block_header().header_extensions) );
+	  header_( "header_extensions", std::move(header_extensions_) );
 	  
 	  /*
 	  header_( "timestamp", self.head_block_header().timestamp.to_timestamp() );
