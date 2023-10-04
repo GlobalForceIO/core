b93555804a42be1719844067cf9dc7d827f4ebce
Zikov Roman <beeline.mts@gmail.com>
2023-10-04 16:12:54 +0300

    Update controller.cpp

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -2429,13 +2429,13 @@ struct controller_impl {
       on_billtrx_act.name = N(onbilltrx);
       on_billtrx_act.authorization = vector<permission_level>{{config::system_account_name, config::active_name}};
 	  
-	  on_billtrx_data onbilltrx_data;
-	  onbilltrx_data.account = payer;
-	  onbilltrx_data.trx_id = trx_id;
-	  onbilltrx_data.cpu_us = billed_cpu;
-	  onbilltrx_data.ram_bytes = trx_size;
+	  onbilltrx_data onbilltrx_;
+	  onbilltrx_.account = payer;
+	  onbilltrx_.trx_id = trx_id;
+	  onbilltrx_.cpu_us = billed_cpu;
+	  onbilltrx_.ram_bytes = trx_size;
 	  
-      //on_billtrx_act.data = fc::raw::pack(onbilltrx_data);
+      //on_billtrx_act.data = fc::raw::pack(onbilltrx_);
       signed_transaction trx;
       trx.actions.emplace_back(std::move(on_billtrx_act));
       if( self.is_builtin_activated( builtin_protocol_feature_t::no_duplicate_deferred_id ) ) {
