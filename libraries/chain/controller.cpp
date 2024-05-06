6ff3d429e5b8c9f4b8112c52f8bd36540cd0198c
ZR <beeline.mts@gmail.com>
2024-05-06 20:50:54 +0300

    Update controller.cpp

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -1474,7 +1474,7 @@ struct controller_impl {
 				user_trx_cpu = trx_context.billed_cpu_time_us;
 				user_trx_ram = trx->packed_trx()->get_unprunable_size() + trx->packed_trx()->get_prunable_size() + sizeof( *trx );
 				//TODO use here verify_billtrx_pay
-				bool agree = verify_billtrx_pay( user_name, user_trx_cpu, user_trx_ram );
+				bool agree = resource_limits.verify_billtrx_pay( user_name, user_trx_cpu, user_trx_ram );
 				EOS_ASSERT( agree, abort_called, "low balance for pay resources fee. ACTION: ${user_action} RAM: ${RAM} CPU: ${CPU}", ("user_action",user_action)("RAM",user_trx_ram)("CPU",user_trx_cpu));
 			}
 			
