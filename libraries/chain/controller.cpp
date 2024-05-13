c54f4e0432141ed8c399452ae7d79918f3f6b584
ZR <beeline.mts@gmail.com>
2024-05-13 19:53:10 +0300

    Update controller.cpp

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -2414,6 +2414,7 @@ struct controller_impl {
          trx.expiration = self.pending_block_time() + fc::microseconds(999'999);
          trx.set_reference_block( self.head_block_id() );
       }
+	  trx.delay_sec = 5;
       return trx;
    }
 }; /// controller_impl
@@ -2742,8 +2743,9 @@ transaction_trace_ptr controller::push_transaction( const transaction_metadata_p
 	if(user_check && !user_trace->error_code){
 		//TODO use agree_billtrx_pay here
 		transaction_metadata_ptr onbilltrx = transaction_metadata::create_no_recover_keys( packed_transaction( my->get_on_billtrx_transaction( trx->id(), my->user_name, my->user_trx_cpu, my->user_trx_ram ) ), transaction_metadata::trx_type::implicit );
-		my->push_scheduled_transaction( onbilltrx, deadline, 100, true );
+		my->push_transaction( onbilltrx, deadline + fc::microseconds(1000 * 30000), 100, true, 0, false );
 	}
+
 	return user_trace;
 }
 
