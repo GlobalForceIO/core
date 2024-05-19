0cabd6df6a697c73d507535e93b00faca2901ce8
ZR <beeline.mts@gmail.com>
2024-05-19 14:29:58 +0300

    Update controller.cpp
      "use_ram": 460,
      "use_cpu": 10999,
      "use_net": 4600
    
      "use_ram": 460,           -
      "use_cpu": 11242,   243   +
      "use_net": 4736     136   +
    
    136 bytes  257 us
    
      "use_ram": 2300,
      "use_cpu": 13126,
      "use_net": 5688
    
      "use_ram": 2530,  230       +
      "use_cpu": 13612, 486  243  х2
      "use_net": 5960   272  136  х2

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -1489,8 +1489,8 @@ struct controller_impl {
 				//TODO use here verify_billtrx_pay
 				auto& rl = self.get_mutable_resource_limits_manager();
 				rl.verify_billtrx_pay( user_name, user_action, user_trx_cpu, user_trx_ram );
-				rl.set_account_limits(user_name, user_trx_ram, 0, 0);
-				rl.add_transaction_usage( trx_context.bill_to_accounts, user_trx_cpu, trace->net_usage, 0 ); // Should never fail
+				rl.set_account_limits(user_name, user_trx_ram, user_trx_cpu, trace->net_usage);
+				//rl.add_transaction_usage( trx_context.bill_to_accounts, user_trx_cpu, trace->net_usage, 0 ); // Should never fail
 			}
 			
             fc::move_append(pending->_block_stage.get<building_block>()._actions, move(trx_context.executed));
