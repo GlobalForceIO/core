c1645f5402ac0cb24f905775c5b47826cdf12b60
ZR <beeline.mts@gmail.com>
2024-05-30 09:24:59 +0300

    Update controller.cpp

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -1367,7 +1367,7 @@ struct controller_impl {
          }
 
          //resource_limits.add_transaction_usage( trx_context.bill_to_accounts, cpu_time_to_bill_us, 0,
-                                                block_timestamp_type(self.pending_block_time()).slot ); // Should never fail
+         //                                       block_timestamp_type(self.pending_block_time()).slot ); // Should never fail
          trace->receipt = push_receipt(gtrx.trx_id, transaction_receipt::hard_fail, cpu_time_to_bill_us, 0);
          trace->account_ram_delta = account_delta( gtrx.payer, trx_removal_ram_delta );
 		 
